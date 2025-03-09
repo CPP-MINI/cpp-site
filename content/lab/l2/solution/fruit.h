eccf15d20a06d98d6b58a9b7dd3a26b532edb433
smogork <krzysztof.smogor@wp.pl>
2025-03-09 16:17:18 +0100

    Add fruit header for enums

---
+++ content/lab/l2/solution/fruit.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace l2 {
+
+    enum class Color{
+        Red, Orange, Green, Fiolet
+    };
+
+    enum class FruitType {
+        Apple, Orange, Plum
+    };
+
+    struct Fruit{
+        Color color;
+        FruitType type;
+    };
+
+    static constexpr Fruit RipeApple {.color = Color::Red, .type = FruitType::Apple };
+    static constexpr Fruit UnripeApple {.color = Color::Green, .type = FruitType::Apple };
+    static constexpr Fruit RipeOrange {.color = Color::Orange, .type = FruitType::Orange }; // C-style enums have problems here
+    static constexpr Fruit UnripeOrange {.color = Color::Green, .type = FruitType::Orange }; 
+    static constexpr Fruit RipePlum {.color = Color::Fiolet, .type = FruitType::Plum };
+    static constexpr Fruit UnripePlum {.color = Color::Green, .type = FruitType::Plum };
+}
