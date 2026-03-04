710d74fe85616568b212a85e70e53ed9553b144e
smogork <krzysztof.smogor@wp.pl>
2026-03-04 18:32:53 +0100

    Simplify code

--- content/lab/l1/solution/bounded_buffer.cpp
+++ content/lab/l1/solution/bounded_buffer.cpp
@@ -2,21 +2,23 @@
 
 #include <iostream>
 
-int buffer_size(const bounded_buffer* buffer) { return buffer->buffer.size(); }
+int buffer_size(const bounded_buffer* buffer){
+    return buffer->buffer.size();
+}
 
-int buffer_capacity(const bounded_buffer* buffer) { return buffer->capacity; }
+int buffer_capacity(const bounded_buffer* buffer){
+    return buffer->capacity;
+}
 
-void flush_buffer(bounded_buffer* buffer)
-{
-    for (const auto& l : buffer->buffer)
-        std::cout << l << std::endl;
+void flush_buffer(bounded_buffer* buffer){
+    for (std::string line : buffer->buffer)
+        std::cout << line << std::endl;
     std::cout << "=====" << std::endl;
     buffer->buffer.clear();
 }
 
-bool add_to_buffer(bounded_buffer* buffer, std::string line)
-{
-    if (buffer_size(buffer) == buffer_capacity(buffer))
+bool add_to_buffer(bounded_buffer* buffer, std::string line){
+    if (buffer_size(buffer) >= buffer_capacity(buffer))
         return false;
 
     buffer->buffer.push_back(line);
